2f1101430e5cb5be395be9e3e2576f565c415b4e
eolavegarc <93262443+eolavegarc@users.noreply.github.com>
2022-04-01 14:26:22 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 let birak = 0
 DFRobotMaqueenPlus.I2CInit()
-DFRobotMaqueenPlus.clearDistance(Motors.M1)
-basic.forever(function () {
-    basic.showNumber(parseFloat(DFRobotMaqueenPlus.readeDistance(Motors1.M1)))
-})
+DFRobotMaqueenPlus.clearDistance(Motors.ALL)
 basic.forever(function () {
     birak = parseFloat(DFRobotMaqueenPlus.readeDistance(Motors1.M1))
-    if (birak < 5) {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+    if (birak < 22.8707945181) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
     } else {
-        DFRobotMaqueenPlus.mototStop(Motors.M1)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
     }
 })
